13c21ac7c1e330baf798b06db969a044fd8ca530
Samuel Gerard <sggerard1997@gmail.com>
2024-11-16 15:10:41 -0800

    Enable roles by default
    Enable roles by default so that users can login using keycloak onto a clean install.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,25 @@
 'use strict';
 
+//Enable all roles by default
+async function enableRoles(strapi){
+  const roleService = strapi.plugin('strapi-plugin-sso').service('role');
+
+  //Check enabled roles haven't already been set
+  const keycloakRoles = await roleService.keycloakRoles();
+  if(!keycloakRoles){
+  
+    console.log(" - Enabling all roles for Keycloak");
+    const adminRoleService = strapi.admin.services.role;
+    const adminRoles = await adminRoleService.find();
+    
+    await roleService.update([
+      {
+        'oauth_type': 1,
+        role: adminRoles.map(r => r.id),
+      }])
+  }
+}
+
 module.exports = async ({ strapi }) => {
   const actions = [
     {
@@ -10,4 +30,5 @@ module.exports = async ({ strapi }) => {
     },
   ];
   await strapi.admin.services.permission.actionProvider.registerMany(actions);
+  await enableRoles(strapi);
 };
